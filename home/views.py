b1f8f72de77b2dfb1c4a3831008c586cad1083d7
akandy080 <akandy080@gmail.com>
2023-04-25 11:34:49 -0700

    update for deploy

--- home/views.py
+++ home/views.py
@@ -369,7 +369,7 @@ def pay(request):
     if request.method == 'POST':
         api_key = 'sk_test_163cc23cf71bc18e4b1a519dcbe4acc0cd94de89'
         curl = 'https://api.paystack.co/transaction/initialize'
-        cburl = 'http://127.0.0.1:8000/callback/'
+        cburl = 'http://16.16.173.200/callback/'
         ref = str(uuid.uuid4())
         profile =  Profile.objects.get(user__username=request.user.username)
         shop_code = profile.id
--- shopping/settings.py
+++ shopping/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^+4a%4!22v5p-2^0m#z&*+_cdgru98@sa3+r*1y1t*g_gt9c6g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['16.16.173.200', '*']
 
 
 # Application definition
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'shopping.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.console.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
@@ -121,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR/ 'static/'
+STATICFILES_DIR = BASE_DIR / 'home/static'
 # STATIC_ROOT = os.path.join(BASE_DIR/ 'static/)
 
 MEDIA_URL = '/media/'
